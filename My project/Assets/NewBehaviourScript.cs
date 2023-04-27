8995bd9622d28b961dcf3b72e21c6fc1378a1c79
JC <jc081810qpang@naver.com>
2023-04-27 15:55:22 +0900

    Crazy Ball Maze
    조작, 3인칭 카메라 수정

---
+++ My project/Assets/NewBehaviourScript.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    Rigidbody rb;
+
+    float Delay = 0f;
+    [SerializeField]
+    GameObject OC;
+    [SerializeField]
+    GameObject TC;
+    private bool awwd;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Input.GetAxisRaw("Horizontal");
+        float z = Input.GetAxisRaw("Vertical");
+
+       
+       
+         Vector3 momo = new Vector3(z, 0, x);
+         //momo = Quaternion.AngleAxis(Camera.main.transform.eulerAngles.y, Vector3.up) * momo;
+         //momo = Quaternion.AngleAxis(Camera.main.transform.eulerAngles.z, Vector3.forward) * momo;
+         rb.AddForce(momo);
+
+
+        if (Input.GetKeyDown(KeyCode.Space) && Delay <= 0)
+        {
+            Delay = 5.0f;
+            awwd = false;
+            StartCoroutine(chageC());
+        }
+        if(awwd == true)
+        {            
+            OC.SetActive(true);
+            TC.SetActive(false);
+        }
+        if (Delay >= 0)
+        {
+            Delay -= Time.deltaTime;
+          
+        }
+    }
+
+    IEnumerator chageC()
+    {   
+        Debug.Log("asdsad");
+        OC.SetActive(false);
+        TC.SetActive(true);
+        yield return new WaitForSeconds(5f);
+        awwd = true;
+    }
+}

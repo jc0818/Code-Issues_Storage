a4efd3ea64d649f4a2bf9d3c90f79951235984ee
JC <jc081810qpang@naver.com>
2023-05-09 14:34:54 +0900

    Update AndroidSuhaengCode.kt
    오브젝트 id셋팅 잘하기

--- AndroidSuhaengCode.kt
+++ AndroidSuhaengCode.kt
@@ -6,28 +6,21 @@ import android.view.MenuItem
 import android.view.View
 import android.widget.ArrayAdapter
 import androidx.appcompat.app.AppCompatActivity
-
 import com.example.suhang.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
-
     val  data1 = arrayListOf("overdose" , "peaches")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         registerForContextMenu(binding.list)
-
         val adapter1 = ArrayAdapter(this,android.R.layout.simple_list_item_1,data1)
         binding.list.adapter = adapter1
-
         binding.list.setOnItemClickListener { adapterView, view, i, l ->
             binding.textView4.text = "${data1[i]}"
-
-
         }
-
         binding.button.setOnClickListener(
             {
                 binding.textView.text = "노래"
@@ -39,7 +32,6 @@ class MainActivity : AppCompatActivity() {
             }
         )
     }
-
     override fun onCreateContextMenu(
         menu: ContextMenu?,
         v: View?,
@@ -54,21 +46,18 @@ class MainActivity : AppCompatActivity() {
             }
         }
     }
-
     override fun onContextItemSelected(item: MenuItem): Boolean {
         when(item.itemId)
         {
             R.id.play -> {
               binding.textView.text = "음악 재생"
             }
-
             R.id.stop -> {
                 binding.textView.text = "음악 멈춤"
             }
         }
         return super.onContextItemSelected(item)
     }
-
     // Build.gradle(module:app)
 
     // buildFeatures{
